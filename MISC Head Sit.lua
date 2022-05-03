898765e854dcc0e41f9389f6086757918c4b47e3
Kenaii11 <78361355+Kenaii11@users.noreply.github.com>
2022-05-03 11:24:37 +0200

    Update MISC Head Sit.lua

--- MISC Head Sit.lua
+++ MISC Head Sit.lua
@@ -1,4 +1,4 @@
 local target = "PlayerName"
 
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[target].Character.Head.CFrame
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[target].Character.Head.CFrame --game.Players[target].Character.HumanoidRootPart.CFrame * CFrame.new(0, 8, 0)
 game.Players.LocalPlayer.Character.Humanoid.Sit = true
