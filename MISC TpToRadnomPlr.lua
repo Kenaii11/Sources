09015d9efdb57e2da2ec18498dc6932d429d66d1
Kenaii11 <78361355+Kenaii11@users.noreply.github.com>
2022-05-03 11:10:48 +0200

    Create MISC TpToRadnomPlr.lua

---
+++ MISC TpToRadnomPlr.lua
@@ -0,0 +1,4 @@
+local randomPlayer = game.Players:GetPlayers()
+[math.random(1,#game.Players:GetPlayers())]
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(randomPlayer.Character.Head.Position.X, randomPlayer.Character.Head.Position.Y, randomPlayer.Character.Head.Position.Z)) 
