d84e9df37f3b2ec51bf53fe4355d6b6f51bcb893
Kenaii11 <78361355+Kenaii11@users.noreply.github.com>
2022-05-03 11:23:37 +0200

    Create MISC Head Sit.lua

---
+++ MISC Head Sit.lua
@@ -0,0 +1,4 @@
+local target = "PlayerName"
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[target].Character.Head.CFrame
+game.Players.LocalPlayer.Character.Humanoid.Sit = true
